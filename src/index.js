9f0a34d30a4ce2e6953bc27b30edaaa3f006eac4
hklee <leehunkyu00@gmail.com>
2020-12-22 16:47:21 +0900

    release version v1.0.0

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const log = require('electron-log');
+const {autoUpdater} = require('electron-updater');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+ let mainWindow = null;
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: { nodeIntegration: true }
   });
 
   // and load the index.html of the app.
@@ -23,7 +33,11 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  log.info('app ready');
+  autoUpdater.checkForUpdates();
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -44,3 +58,35 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+// autoUpdater section
+function sendStatusToWindow(text) {
+  log.info(text);
+  if (mainWindow) {
+    mainWindow.webContents.send('message', text);
+  }
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('update-available');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('update-not-available');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('error' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed : " + progressObj.bytesPerSecond;
+  log_message += " - Download : " + progressObj.percent + "%";
+  log_message += "(" + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', () => {
+  sendStatusToWindow('update-downloaded');
+  autoUpdater.quitAndInstall();
+})
